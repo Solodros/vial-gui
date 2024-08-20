6040d866141f60cae95b2f43f9305aeaff45bfe6
Solodros <34903426+Solodros@users.noreply.github.com>
2024-08-20 17:54:33 +0800

    Update http-server-cors.py

--- http-server-cors.py
+++ http-server-cors.py
@@ -15,8 +15,8 @@ except ImportError: # Python 2
 class CORSRequestHandler (SimpleHTTPRequestHandler):
     def end_headers (self):
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
-        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+#        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+#        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
